2b0a7548975edebd78b0436c4102144ceb82b2fa
Shy Agam <us@bespunky.io>
2021-04-25 01:41:38 +0300

    ➕ Implemented mouse flick ease out animation

--- libs/navigables/camera/reactive/core/reactive-camera.ts
+++ libs/navigables/camera/reactive/core/reactive-camera.ts
@@ -1,10 +1,11 @@
-import { Key                                        } from 'ts-key-enum';
-import { BehaviorSubject, Observable                } from 'rxjs';
-import { exhaustMap, map, takeUntil, withLatestFrom } from 'rxjs/operators';
-import { ElementRef, Injectable                     } from '@angular/core';
-import { DocumentRef                                } from '@bespunky/angular-zen/core';
-import { Property                                   } from '@bespunky/typescript-utils';
+import { Key                                                                           } from 'ts-key-enum';
+import { animationFrameScheduler, BehaviorSubject, combineLatest, interval, Observable } from 'rxjs';
+import { exhaustMap, map, observeOn, switchMap, takeUntil, takeWhile, withLatestFrom   } from 'rxjs/operators';
+import { ElementRef, Injectable                                                        } from '@angular/core';
+import { Property                                                                      } from '@bespunky/typescript-utils';
+import { DocumentRef                                                                   } from '@bespunky/angular-zen/core';
 
+import { useActivationSwitch     } from '@bespunky/angular-cdk/shared';
 import { EventWithModifiers      } from '@bespunky/angular-cdk/reactive-input/shared';
 import { ReactiveMouseService    } from '@bespunky/angular-cdk/reactive-input/mouse';
 import { ReactiveKeyboardService } from '@bespunky/angular-cdk/reactive-input/keyboard';
@@ -31,9 +32,13 @@ export abstract class ReactiveCamera<TItem> extends Camera<TItem>
     public readonly zoomOnPinch   : BehaviorSubject<boolean> = new BehaviorSubject(true as boolean);
     public readonly moveOnFlick   : BehaviorSubject<boolean> = new BehaviorSubject(true as boolean);
     public readonly moveOnDrag    : BehaviorSubject<boolean> = new BehaviorSubject(true as boolean);
+    public readonly flickX        : BehaviorSubject<boolean> = new BehaviorSubject(true as boolean);
+    public readonly flickY        : BehaviorSubject<boolean> = new BehaviorSubject(true as boolean);
 
     // Factors
     public readonly moveFactor             : BehaviorSubject<number>                  = new BehaviorSubject(3);
+    public readonly flickBreaksStrength    : BehaviorSubject<number>                  = new BehaviorSubject(1);
+    public readonly flickSpeed             : BehaviorSubject<number>                  = new BehaviorSubject(30);
     public readonly keyboardModifierFactors: BehaviorSubject<KeyboardModifierFactors> = new BehaviorSubject(DefaultKeyboardModifierFactors);
         
     constructor(private document: DocumentRef, private mouse: ReactiveMouseService, private keyboard: ReactiveKeyboardService, element: ElementRef)
@@ -115,22 +120,73 @@ export abstract class ReactiveCamera<TItem> extends Camera<TItem>
         
         // Listen for drag start, then switch it dragging until dragging ends
         const move = dragStart.pipe(
-            exhaustMap(() => dragging.pipe(takeUntil(dragEnd)))
+            exhaustMap(() => dragging.pipe(takeUntil(dragEnd))),
         );
 
         // Reverse movement to match mouse move and hook
         this.hookPosition(move, e => -e.movementX, 'horizontal');
-        this.hookPosition(move, e => -e.movementY, 'vertical');
+        this.hookPosition(move, e => -e.movementY, 'vertical'  );
+
+        this.hookFlick(dragging, dragEnd, e => -e.movementX, 'horizontal');
+        this.hookFlick(dragging, dragEnd, e => -e.movementY, 'vertical');
     }
 
     private hookPosition<T extends EventWithModifiers>(eventFeed: Observable<T>, getAmount: (event: T) => number, direction: 'horizontal' | 'vertical'): void
     {
-        const move = eventFeed.pipe(
+        const movement = eventFeed.pipe(
             accelerateWithKeyboard(getAmount, this.keyboardModifierFactors),
+            map(([amount]) => amount)
         );
 
-        direction === 'horizontal' ? this.subscribe(move, ([amount]) => this.move(amount, 0)) :
-                                     this.subscribe(move, ([amount]) => this.move(0, amount));
+        direction === 'horizontal' ? this.subscribe(movement, amount => this.move(amount, 0)) :
+                                     this.subscribe(movement, amount => this.move(0, amount));
+    }
+
+    private hookFlick(dragging: Observable<MouseEvent>, dragEnd: Observable<MouseEvent>, getAmount: (event: MouseEvent) => number, direction: 'horizontal' | 'vertical'): void
+    {
+        const flickSwitch = direction === 'horizontal' ? this.flickX : this.flickY;
+            
+        // For every drag end, emit the lastest drag event to ensure movement amount is present
+        const lastMovement = dragEnd.pipe(
+            useActivationSwitch(flickSwitch),
+            withLatestFrom(dragging),
+            map(([, lastDragEvent]) => lastDragEvent)
+        );
+
+        const easeOut = this.easeOutMouseMovement(lastMovement, getAmount);
+
+        direction === 'horizontal' ? this.subscribe(easeOut, amount => this.move(amount, 0)) :
+                                     this.subscribe(easeOut, amount => this.move(0, amount));
+    }
+
+    private easeOutMouseMovement<T extends MouseEvent>(eventFeed: Observable<T>, getMovement: (event: T) => number): Observable<number>
+    {
+        /**
+         * The idea is to get the last mouse movement amount, then repeat the movement decreasing the amount each time
+         * until the amount exceeds zero.
+         */
+        const animationInterval = interval(this.flickSpeed.value);
+
+        // Combine the timer and the breaks strength to have both values when calculating the next movement amount
+        const easeOut = (movement: number) => combineLatest([animationInterval, this.flickBreaksStrength]).pipe(
+            observeOn(animationFrameScheduler),
+            // Calculate how much to decrease the original amount on the next animation step.
+            // To calculate the next decreased movement: acceleration * animationStep
+            // To pull the next value closer to zero:    Math.sign(movement) * animation * animationStep
+            // To prevent a first value of 0:            Math.sign(movement) * animation * (animationStep + 1)
+            map(([animationStep, acceleration]) => Math.sign(movement) * acceleration * (animationStep + 1)),
+            // Calculate the next decreased movement amount
+            map(breaksStrength => movement - breaksStrength),
+            // Take the direction into account and stop the animation when exceeding zero
+            takeWhile(nextMove => movement > 0 ? nextMove > 0 : nextMove < 0)
+        );
+
+        return eventFeed.pipe(
+            // Extract the movement amount from the mouse event
+            map(getMovement),
+            // Cancel any animation previously launched and create a new one with the new amount
+            switchMap(easeOut)
+        );
     }
 
     private hookZoom<T extends EventWithModifiers>(
