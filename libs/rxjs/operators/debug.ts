c15d85531fd1d12b1dffe6466f960ed4b06e7a89
Shy Agam <us@bespunky.io>
2021-04-27 00:09:53 +0300

    🎨 Code layout

--- libs/rxjs/operators/debug.ts
+++ libs/rxjs/operators/debug.ts
@@ -4,9 +4,11 @@ import { tap        } from 'rxjs/operators';
 export function debug<T>(message?: string | ((value: T) => string)): (source: Observable<T>) => Observable<T>
 {
     const log =
-        message ? message instanceof Function ? (value: T) => console.log(message(value))
-                                              : () => console.log(message)
-                : (value: T) => console.log(value);
+        message
+        ? message instanceof Function
+            ? (value: T) => console.log(message(value))
+            : () => console.log(message)
+        : (value: T) => console.log(value);
 
     return (source: Observable<T>) => source.pipe(tap(log));
 }
