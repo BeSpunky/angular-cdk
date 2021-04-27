eed511aeb84dd4d1e6295a008aea0b381fc197b2
Shy Agam <us@bespunky.io>
2021-04-27 19:58:51 +0300

    ➕ Added an aspect ratio property to ViewBounds

--- libs/navigables/camera/shared/view-bounds.ts
+++ libs/navigables/camera/shared/view-bounds.ts
@@ -10,13 +10,15 @@ export class ViewBounds
     public readonly width : number;
     public readonly height: number;
     
+    public readonly aspectRatio: number;
+
     public readonly screenCenterX: number;
     public readonly screenCenterY: number;
 
     constructor(
         public readonly viewPort   : ViewPort,
         public readonly viewCenterX: number,
-        public readonly viewCenterY: number,
+        public readonly viewCenterY: number
     )
     {
         this.left   = viewCenterX - viewPort.width  / 2;
@@ -25,6 +27,8 @@ export class ViewBounds
         this.height = viewPort.height;
         this.right  = this.left + this.width;
         this.bottom = this.top  + this.height;
+
+        this.aspectRatio = this.width / this.height;
 
         this.screenCenterX = viewPort.width  / 2;
         this.screenCenterY = viewPort.height / 2;
