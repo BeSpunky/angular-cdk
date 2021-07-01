2ccc94f566b49bb39720f7556ccf3f3e03c06040
Shy Agam <us@bespunky.io>
2021-07-01 11:19:04 +0300

    📦 Updated storybook

--- libs/timeline/.storybook/preview.js
+++ libs/timeline/.storybook/preview.js
@@ -1,8 +1,3 @@
-import { addDecorator } from '@storybook/angular';
-import { withKnobs } from '@storybook/addon-knobs';
-
-addDecorator(withKnobs);
-
 export const parameters = {
     options: {
         storySort: {
