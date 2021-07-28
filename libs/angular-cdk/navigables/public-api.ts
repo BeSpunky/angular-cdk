14340ad45f0b361cfe1ef88165bb9381d760c14f
Shy Agam <us@bespunky.io>
2021-07-28 14:47:04 +0300

    📁 Moved modules code to their respective secondary entrypoint

--- libs/angular-cdk/navigables/public-api.ts
+++ libs/angular-cdk/navigables/public-api.ts
@@ -1,2 +1,2 @@
-
-
+export * from '@bespunky/angular-cdk/navigables/camera';
+export * from '@bespunky/angular-cdk/navigables/camera/reactive';
--- libs/angular-cdk/timeline/public-api.ts
+++ libs/angular-cdk/timeline/public-api.ts
@@ -1,2 +1,3 @@
-
-
+export * from '@bespunky/angular-cdk/timeline/abstraction';
+export * from '@bespunky/angular-cdk/timeline/core';
+export * from '@bespunky/angular-cdk/timeline/shared';
