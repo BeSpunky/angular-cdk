14a0d8053ed5082387bd99214379aff353655be5
Shy Agam <us@bespunky.io>
2021-07-28 14:49:29 +0300

    ➖ Removed navigables library

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   projects: [
     '<rootDir>/apps/official-site',
-    '<rootDir>/libs/navigables',
     '<rootDir>/libs/rxjs',
     '<rootDir>/apps/demo',
     '<rootDir>/libs/angular-cdk',
--- libs/navigables/README.md
+++
@@ -1,7 +0,0 @@
-# navigables
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test navigables` to execute the unit tests.
