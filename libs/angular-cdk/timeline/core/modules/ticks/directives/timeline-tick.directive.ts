a6974273a38613f1724529a3db2914b5e5262245
Shy Agam <us@bespunky.io>
2021-08-04 23:56:20 +0300

    ♻️ Moved tick rendering responsibility to tick directive, now independent of timeline directive

--- libs/angular-cdk/timeline/core/modules/ticks/directives/timeline-tick.directive.ts
+++ libs/angular-cdk/timeline/core/modules/ticks/directives/timeline-tick.directive.ts
@@ -2,8 +2,9 @@ import { Observable                                      } from 'rxjs';
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 import { TimelineCamera, TimelineConfig                                                                      } from '@bespunky/angular-cdk/timeline/abstraction';
-import { TimelineTick, TickData, DatesBetweenGenerator, DayFactor, TickLabeler, WidthCalculator, TickContext } from '@bespunky/angular-cdk/timeline/abstraction/ticks';
+import { TimelineTick, TickData, DatesBetweenGenerator, DayFactor, TickLabeler, WidthCalculator, TickContext, TimelineTickRenderer } from '@bespunky/angular-cdk/timeline/abstraction/ticks';
 import { TimelineTickVirtualizationService                                                                   } from '../services/virtualization/timeline-tick-virtualization.service';
+import { filter } from 'rxjs/operators';
 
 /**
  * Converts an element to a tick template and provides tools for timelines to easily render ticks with
@@ -24,11 +25,12 @@ export class TimelineTickDirective extends TimelineTick
     public readonly itemsToRender: Observable<TickData[]>;
     
     constructor(
-        public  readonly view      : ViewContainerRef,
-        public  readonly template  : TemplateRef<TickContext>,
-        public  readonly config    : TimelineConfig,
-        public  readonly camera    : TimelineCamera,
-        private readonly virtualize: TimelineTickVirtualizationService
+        public  readonly view        : ViewContainerRef,
+        public  readonly template    : TemplateRef<TickContext>,
+        public  readonly config      : TimelineConfig,
+        public  readonly camera      : TimelineCamera,
+        private readonly virtualize  : TimelineTickVirtualizationService,
+        private readonly tickRenderer: TimelineTickRenderer,
     )
     {
         super();
@@ -36,6 +38,12 @@ export class TimelineTickDirective extends TimelineTick
         this.shouldRender  = this.virtualize.shouldRenderFeed(this);
         this.width         = this.virtualize.widthFeed(this);
         this.itemsToRender = this.virtualize.itemsToRenderFeed(this);
+
+        const render   = this.itemsToRender;
+        const unrender = this.shouldRender.pipe(filter(shouldRender => !shouldRender));
+
+        this.subscribe(render  , renderedItems => this.tickRenderer.renderTicks  (this, renderedItems));
+        this.subscribe(unrender, ()            => this.tickRenderer.unrenderTicks(this));
     }
 
     static ngTemplateContextGuard(directive: TimelineTickDirective, context: TickContext): context is TickContext { return true; }
