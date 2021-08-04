0494b78c688997b5373fbf38c5f09f165d63bec3
Shy Agam <us@bespunky.io>
2021-08-05 00:44:27 +0300

    ➕ Finished ticks component implementation

--- apps/tiket/src/app/modules/routes/routes-list/routes-timeline/routes-timeline.component.ts
+++ apps/tiket/src/app/modules/routes/routes-list/routes-timeline/routes-timeline.component.ts
@@ -1,21 +1,21 @@
-import { Component, Input } from '@angular/core';
-import { eachDayOfInterval, eachHourOfInterval, differenceInMinutes, differenceInSeconds, startOfSecond, startOfMinute, addMinutes, addSeconds, setHours, setMinutes } from 'date-fns';
-import { AssignedTiketsByTiker } from '../../../../shared/services/routes-coordinator.service';
+import { Component, Input } from '@angular/core'
+import { eachDayOfInterval, eachHourOfInterval, differenceInMinutes, differenceInSeconds, startOfSecond, startOfMinute, addMinutes, addSeconds, setHours, setMinutes, format } from 'date-fns'
 
-const DayParts       = ['Night', 'Morning', 'Noon', 'Evening'];
-const HourPartsCount = 4;
+import { AssignedTiketsByTiker } from '../../../../shared/services/routes-coordinator.service'
+
+const DayParts       = ['Night', 'Morning', 'Noon', 'Evening']
+const HourPartsCount = 3
 
 function divideIntoParts(dates: Date[], intervalSize: number, partCount: number, setPart: (date: Date, partValue: number) => Date): Date[]
 {
-    const partPlaceholders = Array.from({ length: partCount });
-    const partSize         = intervalSize / partCount;
+    const partPlaceholders = Array.from({ length: partCount })
+    const partSize         = intervalSize / partCount
 
     return dates.reduce<Date[]>((parts, date) => [
         ...parts,
         ...partPlaceholders.map((_, index) => setPart(date, index * partSize))
-    ], []);
+    ], [])
 }
-
 @Component({
     selector   : 'tt-routes-timeline',
     templateUrl: './routes-timeline.component.html',
@@ -23,41 +23,35 @@ function divideIntoParts(dates: Date[], intervalSize: number, partCount: number,
 })
 export class RoutesTimelineComponent
 {    
-    @Input() public assignedTikets: AssignedTiketsByTiker = [];
+    @Input() public assignedTikets: AssignedTiketsByTiker = []
 
-    now = new Date();
-    zoom = 122;
+    now = new Date()
+    zoom = 122
 
     public dayFactors = {
         day         : 1,
         dayParts    : 1 / 4,
         hoursInDay  : 1 / (24),
-        hourParts   : 1 / (24 * 60 / HourPartsCount),
+        hourParts   : 1 / (24 * 60 * HourPartsCount),
         minutesInDay: 1 / (24 * 60),
         secondsInDay: 1 / (24 * 60 * 60)
-    } as const;
+    } as const
 
     public datesBetween = {
         days     : (start: Date, end: Date) => eachDayOfInterval ({ start, end }),
         dayParts : (start: Date, end: Date) => divideIntoParts(eachDayOfInterval({ start, end }), 24, DayParts.length, setHours),
         hours    : (start: Date, end: Date) => eachHourOfInterval({ start, end }),
-        hourParts: (start: Date, end: Date) => divideIntoParts(eachDayOfInterval({ start, end }), 60, HourPartsCount, setMinutes),
+        hourParts: (start: Date, end: Date) => divideIntoParts(eachHourOfInterval({ start, end }), 60, HourPartsCount, setMinutes),
         minutes  : (start: Date, end: Date) => Array.from({ length: Math.abs(differenceInMinutes(start, end)) }, (_, minIndex) => startOfMinute(addMinutes(start, minIndex))),
         seconds  : (start: Date, end: Date) => Array.from({ length: Math.abs(differenceInSeconds(start, end)) }, (_, secIndex) => startOfSecond(addSeconds(start, secIndex)))
-    } as const;
+    } as const
     
     public labels = {
         days     : (value: Date) => value.getDate(),
         dayParts : (value: Date) => DayParts[Math.floor(value.getHours() / 6)],
         hours    : (value: Date) => value.getHours(),
-        hourParts: (value: Date) => value.getMinutes(),
+        hourParts: (value: Date) => format(value, 'hh:mm'),
         minutes  : (value: Date) => value.getMinutes(),
         seconds  : (value: Date) => value.getSeconds()
-    } as const;
-    
-    public asd(start: Date, end: Date)
-    {
-        return eachDayOfInterval({ start, end });
-    }
-
+    } as const
 }
