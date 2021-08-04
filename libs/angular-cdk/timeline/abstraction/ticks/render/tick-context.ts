52b2c6560983e648e924f1b972ff89d6d25b6065
Shy Agam <us@bespunky.io>
2021-08-04 14:15:37 +0300

    ♻️ Simplified tick context by converting subjects to simple values

--- libs/angular-cdk/timeline/abstraction/ticks/render/tick-context.ts
+++ libs/angular-cdk/timeline/abstraction/ticks/render/tick-context.ts
@@ -1,37 +1,23 @@
-import { BehaviorSubject } from 'rxjs';
-
 import { TickItem } from './tick-item';
 
 export class TickContext
 {
-    public readonly width!          : BehaviorSubject<number>;
-    public readonly positionX!      : BehaviorSubject<number>;
-    public readonly positionY!      : BehaviorSubject<number>;
-    public readonly screenPositionX!: BehaviorSubject<number>;
-    public readonly screenPositionY!: BehaviorSubject<number>;
-    public readonly label!          : BehaviorSubject<string | number>;
-    public readonly value!          : BehaviorSubject<Date>;
+    public readonly width!          : number;
+    public readonly positionX!      : number;
+    public readonly positionY!      : number;
+    public readonly screenPositionX!: number;
+    public readonly screenPositionY!: number;
+    public readonly label!          : string | number;
+    public readonly value!          : Date;
     
     constructor(item: TickItem)
     {
-        this.positionX       = new BehaviorSubject(item.positionX      );
-        this.positionY       = new BehaviorSubject(item.positionY      );
-        this.screenPositionX = new BehaviorSubject(item.screenPositionX);
-        this.screenPositionY = new BehaviorSubject(item.screenPositionY);
-        this.value           = new BehaviorSubject(item.value          );
-        this.width           = new BehaviorSubject(item.width          );
-        this.label           = new BehaviorSubject(item.label          );
+        this.update(item);
     }
 
     public update(item: TickItem)
     {
-        this.positionX      .next(item.positionX      );
-        this.positionY      .next(item.positionY      );
-        this.screenPositionY.next(item.screenPositionY);
-        this.screenPositionX.next(item.screenPositionX);
-        this.value          .next(item.value          );
-        this.width          .next(item.width          );
-        this.label          .next(item.label          );
+        Object.assign(this, item);
     }
 }
 
