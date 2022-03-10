7cd39a0f4f9873b1002fcc4b828dbcffae2a2657
Shy Agam <us@bespunky.io>
2022-03-10 19:09:38 +0200

    ➖ Removed old file

--- libs/angular-cdk/timeline/well-known-ticks/public-api.ts
+++
@@ -1,9 +0,0 @@
-export * from './services/location/timeline-location-utils';
-
-export * from './tools/unfucked-date';
-export * from './tools/date-consts';
-export * from './tools/date-functions';
-
-export * from './types';
-export * from './utils';
-export * from './defaults';
